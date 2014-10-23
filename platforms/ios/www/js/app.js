95ae48e4585b9fae993cf63975791192a441ca05
Alex Kuang <uoroaix@gmail.com>
2014-10-23 13:32:18 -0700

    testing phonegap build

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -2,13 +2,25 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+
+    var slider = new PageSlider($('body'));
+
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
-    EmployeeListView.prototype.template = 
-            Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
-        console.log("Service initialized");
-        $('body').html(new HomeView(service).render().$el);
+      router.addRoute('', function() {
+          slider.slidePage(new HomeView(service).render().$el);
+      });
+
+      router.addRoute('employees/:id', function(id) {
+          service.findById(parseInt(id)).done(function(employee) {
+              slider.slidePage(new EmployeeView(employee).render().$el);
+          });
+      });
+
+      router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
